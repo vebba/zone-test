fee0dfc8faf089af7b471793e6833ac6afa314c2
niceone <pawel@niceone.io>
2018-08-13 18:31:24 +0200

    api key remove form code

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -9,7 +9,9 @@ import {
 
 export const api = (url) => fetch(url).then(response => response.json());
 
-const MOVIES_API_KEY = process.env.API_KEY || '37051ea49d70092be42ce6efa129fe6d';
+const MOVIES_API_KEY = process.env.REACT_APP_API_KEY || process.env.API_KEY;
+
+console.log(process.env);
 
 const GENRES_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${MOVIES_API_KEY}&language=en-US`;
 const MOVIES_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${MOVIES_API_KEY}&language=en-US&page=1`;
